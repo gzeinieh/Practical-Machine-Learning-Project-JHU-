2e7aa5622194c12f5249b70a3e9a9d5637edb94e
gzeinieh <zeiniehgeorge@gmail.com>
2016-01-24 13:52:49 -0600

    “final”

--- Code.R
+++ Code.R
@@ -1,9 +1,14 @@
-```R
+# getting and cleaning data
+
 DataT<- read.csv(url("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"),na.strings=c("","NA"))
 
 DataT1 <- DataT[ , colSums(is.na(DataT)) == 0]; DataTfinal <- DataT1[,-c(1:8)];
 
-install.packages('randomForest'); library(randomForest); libaray(caret); 
+
+
+install.packages('randomForest'); library(randomForest); libaray(caret);
+
+# data spliting
 
 inTrain <- createDataPartition(DataTfinal$classe,list=FALSE)
 
@@ -11,11 +16,17 @@ training <- DataTfinal[inTrain,]
 
 testing <- DataTfinal[-inTrain,]
 
+# training with randomforest 
+
 modelfit <- randomForest(classe~., data=training, method='class')
+
+# testing
 
 pred <- predict(modelfit,testing)
 
 confusionMatrix(pred,testing$classe)
+
+# prediction
 
 DataV<- read.csv(url("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"),na.strings=c("","NA"))
 
@@ -25,4 +36,3 @@ DataVfinal <- DataV1[,-c(1:8)]
 
 predfinal <- predict(modelfit,DataVfinal) 
 
-```
